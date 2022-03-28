77cecbe55c59aa3a298c8dbbd70ddc43d7c7f23c
arifnasrul <101873296+arifnasrul@users.noreply.github.com>
2022-03-28 14:28:00 +0700

    four commit

---
+++ nama-app/src/App.jsx
@@ -0,0 +1,12 @@
+import PlaylistContainer from './components/playlistpertama';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="title">  --- CREATE PLAYLIST ---  </h1>
+      <PlaylistContainer />
+    </div>
+  );
+}
+
+export default App;
