7cf830fde990ba3e6643d5e68638121ab2747554
arifnasrul <101873296+arifnasrul@users.noreply.github.com>
2022-04-08 06:32:09 +0700

    homework react router

--- src/Redux/userToken.js
+++ src/Redux/userToken.js
@@ -3,16 +3,16 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
     token: "",
 }
-
 export const userTokenSlice = createSlice({
     name: 'userToken',
     initialState,
     reducers: {
         saveToken: (state, action) => {
-        state.token += action.payload
+            state.token = action.payload
         },
     },
 })
 
 export const { saveToken } = userTokenSlice.actions
+
 export default userTokenSlice.reducer
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Assets/index.css';
-import App from './Pages';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { store } from './Redux/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
