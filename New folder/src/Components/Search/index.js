9aac9f882fa553c9d7714a116008a52a075c204d
arifnasrul <101873296+arifnasrul@users.noreply.github.com>
2022-04-14 22:32:41 +0700

    antd

---
+++ New folder/src/Components/Search/index.js
@@ -0,0 +1,25 @@
+import "../../Assets/Search.css";
+import PropTypes from "prop-types";
+import React from "react";
+import { Input } from "antd";
+
+SearchBar.propTypes = {
+	handleInput: PropTypes.func,
+	handleSearch: PropTypes.func,
+};
+
+function SearchBar({handleInput, handleSearch}) {
+
+	const { Search } = Input;
+
+	return (
+		<>
+			<form className='SearchBar' onSubmit={handleSearch}>
+				<Search placeholder="input search text" onChange={handleInput} />
+			</form>
+		</>
+	);
+    
+}
+
+export default SearchBar;
